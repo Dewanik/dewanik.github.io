a80c38c2c57ba57cc6d85e2969ef2d18ea623d92
dewanik <kdewanik@gmail.com>
2024-07-31 10:39:04 -0400

    threejs

---
+++ src/app/components/threescene.js
@@ -0,0 +1,44 @@
+// components/ThreeScene.js
+"use client";
+
+import React, { useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Text } from '@react-three/drei';
+import { useThree } from '@react-three/fiber';
+
+const RotatingText = () => {
+  const textRef = useRef();
+  const { mouse } = useThree();
+
+  useFrame(() => {
+    if (textRef.current) {
+      textRef.current.rotation.y = mouse.x * 2;
+      textRef.current.rotation.x = -mouse.y * 2;
+    }
+  });
+
+  return (
+    <Text
+      ref={textRef}
+      fontSize={1}
+      color="white"
+      position={[0, 0, 0]}
+      anchorX="center"
+      anchorY="middle"
+    >
+      Yantra Inc
+    </Text>
+  );
+};
+
+const ThreeScene = () => {
+  return (
+    <Canvas style={{ width: '100%', height: '100vh', background: '#000000' }}>
+      <ambientLight intensity={0.5} />
+      <pointLight position={[10, 10, 10]} />
+      <RotatingText />
+    </Canvas>
+  );
+};
+
+export default ThreeScene;
