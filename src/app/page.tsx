506f3b40afcec9a5c268fef86a96bdf0b18c8e58
Dewanik Koirala <36784941+Dewanik@users.noreply.github.com>
2024-07-31 11:29:12 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,12 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Next.js with Three.js</title>
-        <meta name="description" content="A simple landing page with Next.js and Three.js" />
+        <title>Yantra Inc</title>
+        <meta name="description" content="A simple landing page with Next.js and Three.js of Yantra Inc, Software Company from Birtamode, Jhapa, Nepal" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header style={{ position: 'absolute', top: 0, width: '100%', padding: '10px 20px', background: 'rgba(255, 255, 255, 0.8)', zIndex: 10 }}>
-        <h1>My 3D Landing Page</h1>
-      </header>
+  
 
       <main style={{ margin: 0, padding: 0 }}>
         <ThreeScene />
